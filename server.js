98c778d8f49d39bf5dc1488a58d92da6695e3080
Jonathan Dahan <jonathan.dahan@buglabs.net>
2011-05-22 06:51:41 -0400

    cleanup function lines

--- server.js
+++ server.js
@@ -48,136 +48,150 @@ var foursquare_id = 'JAQQS11NSBCQEP3RBZAVITCME54S3FSCWAZ1204KS1TMNRJY'
 var db = mongoose.connect('mongodb://localhost/soundscape');
 
 // get a list of the most likely venues
-app.get('/venue/:list/:ll', function(req, res) {
-  request({uri:'https://api.foursquare.com/v2/venues/search?client_id='+foursquare_id+'&client_secret='+foursquare_secret+'&ll='+req.params.ll+'&limit=10'}
-  , function (error, response, body) {
+app.get('/venue/:list/:ll'
+, function(req, res) {
+    request({uri:'https://api.foursquare.com/v2/venues/search?client_id='+foursquare_id+'&client_secret='+foursquare_secret+'&ll='+req.params.ll+'&limit=10'}
+    , function (error, response, body) {
 
-      Venues = mongoose.model('Venues');
-      var venues = new Array();
+        Venues = mongoose.model('Venues');
+        var venues = new Array();
       
-      if (!error && response.statusCode == 200) {
-        // grab possible venues from underscore
-        var vs = underscore.first(underscore.select(JSON.parse(body).response.groups, function(group) { return group.type == req.params.list })).items;
-        for ( var i=0, len=vs.length; i<len; ++i ){
-          var v = vs[i];
-          var venue = Venues.findById(v.id);
-          if(venue.name == null) {
-            venue = new Venues();
-            venue.name = v.name;
-            venue.distance = v.location.distance;
-            var cat = underscore.first(v.categories);
-            if( cat != null ) {
-              venue.category = cat.name;
-              venue.icon_uri = cat.icon;
-            }
-            venue.save();
-          } 
-          venues.push(venue);
+        if (!error && response.statusCode == 200) {
+          // grab possible venues from underscore
+          var vs = underscore.first(underscore.select(JSON.parse(body).response.groups, function(group) { return group.type == req.params.list })).items;
+          for ( var i=0, len=vs.length; i<len; ++i ){
+            var v = vs[i];
+            var venue = Venues.findById(v.id);
+            if(venue.name == null) {
+              venue = new Venues();
+              venue.name = v.name;
+              venue.distance = v.location.distance;
+              var cat = underscore.first(v.categories);
+              if( cat != null ) {
+                venue.category = cat.name;
+                venue.icon_uri = cat.icon;
+              }
+              venue.save();
+            } 
+            venues.push(venue);
+          }
         }
-      }
-      res.send(venues);
-    })
-});
+        res.send(venues);
+      })
+  }
+);
 
 var echonest_key = '3XHF2NDEZOK0Y1CLM';
 // get a song
 app.get('/song/:code'
-  , function(req, res) {
-  var code = req.params.code;
-  request({uri: 'http://developer.echonest.com/api/v4/song/identify?api_key='+echonest_key+'&code='+code}
-  , function (error, response, body) {
-      Song = mongoose.model('Song');
-      var song = null;
+, function(req, res) {
+    var code = req.params.code;
+    request({uri: 'http://developer.echonest.com/api/v4/song/identify?api_key='+echonest_key+'&code='+code}
+    , function (error, response, body) {
+        Song = mongoose.model('Song');
+        var song = null;
 
-      if (!error && response.statusCode == 200) {
+        if (!error && response.statusCode == 200) {
 
-        var songs = JSON.parse(body).response.songs;
+          var songs = JSON.parse(body).response.songs;
 
-        // Default to the best song ever if none was found
-        if ( songs.size == 0 ) {
-          song = new Song();
-          song.id = 'RICKASTLEY';
-          song.title = 'Never gonna give you up';
-          song.artist = 'Rick Astley';
-          song.save();
+          // Default to the best song ever if none was found
+          if ( songs.size == 0 ) {
+            song = new Song();
+            song.id = 'RICKASTLEY';
+            song.title = 'Never gonna give you up';
+            song.artist = 'Rick Astley';
+            song.save();
+          }
+
+          // Try and load a cached version of the song
+          song = Song.findById(underscore.first(songs).id);
+
+          // If the cache misses, add new info
+          if ( song.title == null ) {
+            song = new Song();
+            song.title = underscore.first(songs).title
+            song.artist = underscore.first(songs).artist_name
+            song.id = underscore.first(songs).id
+            song.save();
+          }
+
+          res.send(song);
         }
+     }
+   )
+  }
+);
 
-        // Try and load a cached version of the song
-        song = Song.findById(underscore.first(songs).id);
-
-        // If the cache misses, add new info
-        if ( song.title == null ) {
-          song = new Song();
-          song.title = underscore.first(songs).title
-          song.artist = underscore.first(songs).artist_name
-          song.id = underscore.first(songs).id
-          song.save();
-        }
-
-        res.send(song);
-      }
-   })
-});
-
-// tag a song
-app.get('/tag/:user/:song/:venue', function(req, res) {
-  Tags = mongoose.model('Tags');
-  var tag = new Tags();
-
-  tag.user = req.params.user;
-  tag.venue = req.params.venue;
-  tag.song = req.params.song;
-
-  tag.save(basic_error_handling)
-}); 
+// the user (dis)liked the song at venue
+app.get('/tag/:user/:like/:song/:venue'
+, function(req, res) {
+    Tags = mongoose.model('Tags');
+    var tag = new Tags();
+    tag.user = req.params.user;
+    tag.venue = req.params.venue;
+    tag.song = req.params.song;
+    tag.like = true;
+    if (req.params.like==0) { tag.like = false; }
+    tag.save(basic_error_handling)
+  }
+); 
 
 // get a user's tags
-app.get('/user/:email/tags', function(req, res) {
-  Tags = mongoose.model('Tags');
-  var tags = Tags.find({'user': req.params.email}, {}, {}, lol_error_handling);
-  res.send(tags);
-});
+app.get('/user/:email/tags'
+, function(req, res) {
+    Tags = mongoose.model('Tags');
+    var tags = Tags.find({'user': req.params.email}, {}, {}, lol_error_handling);
+    res.send(tags);
+  }
+);
 
 // add a user
-app.get('/user/:email/:name/:phone', function(req, res) {
-  Person = mongoose.model('Person');
+app.get('/user/:email/:name/:phone'
+, function(req, res) {
+    Person = mongoose.model('Person');
 
-  var person = Person.find({'email': req.params.email}, {}, {}, lol_error_handling);
+    var person = Person.find({'email': req.params.email}, {}, {}, lol_error_handling);
 
-  if ( person == null ) {
-    person = new Person();
+    if ( person == null ) {
+      person = new Person();
+      person.name = req.params.name;
+      person.email = req.params.email;
+      person.phone = req.params.phone;
+      person.save(basic_error_handling); 
+    }
 
-    person.name = req.params.name;
-    person.email = req.params.email;
-    person.phone = req.params.phone;
-  
-    person.save(basic_error_handling); 
+    res.send(person);
   }
-
-  res.send(person);
-});
+);
 
 // find user 
-app.get('/user/:email', function(req, res) {
-  Person = mongoose.model('Person');
-  var person = Person.find({'email': req.params.email}, {}, {}, lol_error_handling);
-  res.send(person);
-});
+app.get('/user/:email'
+, function(req, res) {
+    Person = mongoose.model('Person');
+    var person = Person.find({'email': req.params.email}, {}, {}, lol_error_handling);
+    res.send(person);
+  }
+);
 
 
 // get the last tags of a venue
-app.get('/venue/:name/tags', function(req, res) {
-  Venues = mongoose.model('Venues');
-  var tags = Tags.find({'name': req.params.name}, {}, {'limit': 10}, lol_error_handling);
-  res.send(tags);
-});
+app.get('/venue/:name/tags'
+, function(req, res) {
+    Venues = mongoose.model('Venues');
+    var tags = Tags.find({'name': req.params.name}, {}, {'limit': 10}, lol_error_handling);
+    res.send(tags);
+  }
+);
 
 // get venue info
-app.get('/venue/:name', function(req, res) {
-  Venues = mongoose.model('Venues');
-  var venues = Venues.find({'name': req.params.name}, {}, {}, lol_error_handling)
-  res.send(venues)
-});
+app.get('/venue/:name'
+, function(req, res) {
+    Venues = mongoose.model('Venues');
+    var venues = Venues.find({'name': req.params.name}, {}, {}, lol_error_handling)
+    res.send(venues)
+  }
+);
 
 // basic error handling
 var basic_error_handling = function(err) {
